0f78e7d147efa5aeea25b98bc8290b77775ac370
Boisxon <boisxon357@gmail.com>
2025-01-16 14:53:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// const ovoz = +prompt("son kiriting")
+
+// function bot (a){
+//     if(a == 0){
+//         alert("son krit")
+//     }
+//     else if(a % 2 == 0){
+//         alert("juft son")
+//     }else if(a %  3){
+//         alert("toq son")
+//     }
+// }
+// bot(ovoz)
+
+function nav__btn1(){
+    document.body.style.backgroundColor = "rgb(196, 181, 165)"
+}
+function nav__btn2(){
+    document.body.style.backgroundColor = "black"
+    document.querySelector("#nav-img")
+    nav-img.addEventListener("click", function() {
+        heading.body.style.color = "white";
+    });
+}
+
+
+
+
